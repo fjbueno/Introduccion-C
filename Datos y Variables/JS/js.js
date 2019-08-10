6befed4710e13bf2f193d4b3d7717bb7dfa643b2
Francisco Javier Bueno Vasquez <franciscojavier.bueno@bbva.com>
2019-08-10 02:08:56 -0500

    Commit 1_3

---
+++ Datos y Variables/JS/js.js
@@ -0,0 +1,58 @@
+/* 
+Modulo: Programacion Avanzada
+Tópico: Introduccion a JavaScript
+Laboratorio Nacional de Informatica Avanzada
+MRySI. Francisco Javier Bueno Vasquez
+Correo. fbueno.mrysi18@lania.edu.mx
+*/
+
+//Tema. Variables
+
+var a;
+console.log("Variable Undefined: " + typeof a);				// Variable "undefined"
+
+b = "BBVA";
+console.log("Variable String: " + typeof b);				// Variable "string"
+
+c = 42;
+console.log("Variable Number: " + typeof c);				// Variable "number"
+
+d = true;
+console.log("Variable Boolean: " + typeof d);				// Variable "boolean"
+
+e = null;
+console.log("Variable Object: " + typeof e);				// Variable "object"
+
+f = undefined;
+console.log("Variable Undefined: " + typeof f);				// Variable "undefined"
+
+g = { banco: "BBVA" };
+console.log("Variable Object: " + typeof g);				// Variable "object"
+
+//Tema. Objetos
+
+let obj = {
+	banco :  "BBVA" ,
+	sucursales :  42 ,
+	division :  "Metro 1"
+};
+
+console.log(obj.banco);		// BBVA
+console.log(obj.sucursales); //42 
+console.log(obj.division);	   // Metro 1
+
+console.log(obj [ "banco" ]);	// BBVA 
+console.log(obj [ "sucursales" ]);	// 42 
+console.log(obj [ "division" ]);	// Metro 1
+
+//Tema. Arrays
+
+var informacionBBVA = [ "BBVA", 42, "Metro 1"];
+
+console.log(informacionBBVA[0]);			// BBVA
+console.log(informacionBBVA[1]);			// 42
+console.log(informacionBBVA[2]);			// Metro 1
+console.log(informacionBBVA.length);		// 3
+
+console.log(typeof informacionBBVA);		// Object
+
